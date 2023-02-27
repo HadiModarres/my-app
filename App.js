b46f08a2ceeb9a35a05628558bfc10f25d777448
Hadi Modarres <modarres.zadeh@gmail.com>
2023-02-27 14:57:25 +1100

    add reanimated example

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {useEffect, useState} from "react";
+import Animated, {Layout} from "react-native-reanimated";
 
 export default function App() {
+  const [y,setY] = useState(5)
+  useEffect(()=>{
+    const interval = setInterval(()=>{
+      setY(y=>y+15);
+    }, 100);
+    return ()=>{
+      clearInterval(interval)
+    }
+  })
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View  style={styles.container}>
+      <Animated.View layout={Layout} style={{position:'absolute', top: 10+y}}>
+        <Text >Open up App.js to start working on your app!</Text>
+      </Animated.View>
       <StatusBar style="auto" />
     </View>
   );
